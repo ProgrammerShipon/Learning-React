5b76ea986f2d961cf0a591ec19932f1735ef41ab
Shipon Hossen Raju <msshipon234@gmail.com>
2023-01-16 19:59:12 +0600

    introducing State store

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,24 +3,41 @@ import React, { Component } from "react";
 class App extends Component {
   // constructor(props) {
   //   super(props);
-  //   this.count = 5;
+  //   // this.count = 5;
+  //   this.state = {
+  //     count: 1,
+  //   };
   // }
-  count = 0;
+  // count = 1;
+
+  state = {
+    count: 1,
+  };
 
   render() {
     // console.log(this.count);
     return (
       <div className="App">
         <h1> Pass Function As Props </h1>
-        <h2> Count = {this.count} </h2> {/* to live in the feature */}
+        <h2> Count = {this.state.count} </h2>
         <button
           onClick={() => {
-            this.count++;  
-            console.log("clicked", this.count);
+            // this.count++;
+            this.setState({ count: this.state.count + 1 });
+            console.log("clicked", this.state.count);
+          }}
+        >
+          add +1
+        </button>{" "}
+        {"   "}
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count - 1 });
+            console.log("clicked", this.state.count);
           }}
         >
           {" "}
-          add +1{" "}
+          sub -1{" "}
         </button>
       </div>
     );
