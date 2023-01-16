d2de498fee12106fd6a5cbd57268068ba1627a57
Shipon Hossen Raju <msshipon234@gmail.com>
2023-01-16 14:02:35 +0600

    Modify profile component using Props

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,20 +3,24 @@ import React, { Component } from "react";
 import Profile from "./profile";
 // import Skills from "./profile/Skills";
 
-import MyProps from "./props";
+// import MyProps from "./props";
+import Bio from "./profile/Bio";
+import Skills from "./profile/Skills";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Profile />
-        {/* <div style={{ marginTop: "30", marginBottom: "30px" }}>
+        <Bio name="Test User" title="Test Title" />
+        <div style={{ marginTop: "30", marginBottom: "30px" }}>
           <h3 style={{ marginBottom: "20px" }}>List of Programmers</h3>
-        </div> */}
-        {/* <h1> Mr. Rana </h1>
-        <Skills />
-        <h1> Mr. Sifat </h1>
-        <Skills /> */}
+        </div>
+        <h2> Mr. Rana </h2>
+        <Skills skillA="React" skillB="Angular" skillC="Vue" />
+        <h2> Mr. Sifat </h2>
+        <Skills skillA="NodeJS" skillB="ExpressJS" skillC="MongoDB" />
+
         {/* <MyProps name="Shipon Hossen Raju" />
         <MyProps name="Arman Ali" />
         <MyProps name=" Saiful Islam" /> */}
