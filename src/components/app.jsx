aeb0f37a93065ea241b38596806eccf10c353a97
Shipon Hossen Raju <msshipon234@gmail.com>
2023-01-16 19:23:30 +0600

    why Need State

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,25 +1,27 @@
 import React, { Component } from "react";
 
-class Child extends Component {
-
-  render() {
-    this.props.func(this);
-    return <h1> I am Child </h1>
-  }
-}
-
 class App extends Component {
-
-  getContext(context) {
-    console.log(context);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.count = 5;
+  // }
+  count = 0;
 
   render() {
-    this.getContext(this);
+    // console.log(this.count);
     return (
       <div className="App">
         <h1> Pass Function As Props </h1>
-        <Child func={this.getContext} />
+        <h2> Count = {this.count} </h2> {/* to live in the feature */}
+        <button
+          onClick={() => {
+            this.count++;  
+            console.log("clicked", this.count);
+          }}
+        >
+          {" "}
+          add +1{" "}
+        </button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/app";
-import Children_Props from "./components/Children-Props"
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Children_Props />
+    <App />
   </React.StrictMode>
 );
 
