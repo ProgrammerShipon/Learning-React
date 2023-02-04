aa2b609db66174b7e85ee46eb95a67972ed856ff
Shipon Hossen Raju <msshipon234@gmail.com>
2023-02-05 01:18:51 +0600

    simple timer fanction complite

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,25 +1,11 @@
 import React, { Component } from "react";
-
-import Profile from "./profile";
-// import Skills from "./profile/Skills";
-
-import MyProps from "./props";
+import Apps from "./Simple-Timer-App/app";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Profile />
-        {/* <div style={{ marginTop: "30", marginBottom: "30px" }}>
-          <h3 style={{ marginBottom: "20px" }}>List of Programmers</h3>
-        </div> */}
-        {/* <h1> Mr. Rana </h1>
-        <Skills />
-        <h1> Mr. Sifat </h1>
-        <Skills /> */}
-        {/* <MyProps name="Shipon Hossen Raju" />
-        <MyProps name="Arman Ali" />
-        <MyProps name=" Saiful Islam" /> */}
+        <Apps/>
       </div>
     );
   }
