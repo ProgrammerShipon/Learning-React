332601d233805690bde6ea70517a0cd987e90f0a
Shipon Hossen Raju <msshipon234@gmail.com>
2023-02-07 21:45:12 +0600

    inline style and extranal style in React

--- src/components/stylePractice/inlineStyles.jsx
+++ src/components/stylePractice/inlineStyles.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import "./inline.style.css";
+
 const myHeader = {
   color: "blue",
   fontWeight: 300,
@@ -12,6 +14,26 @@ class InlineStyles extends Component {
     return (
       <div className="inlineStyles">
         <h1 style={myHeader}> How to style React App </h1>
+        <h1 style={myHeader}> Another Header Tag </h1>
+        <h1
+          style={{
+            color: "blue",
+            fontWeight: 300,
+            fontFamily: "Arial",
+            fontSize: "36px",
+          }}
+        >
+          {" "}
+          Inline style Header Tag{" "}
+        </h1>
+
+        <button> Click Me A </button>
+        
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo dolor provident nostrum error possimus! Accusantium.</p>
+        <button> link me B </button>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo dolor provident nostrum error possimus! Accusantium.</p>
+        <button> link me C </button>
       </div>
     );
   }
